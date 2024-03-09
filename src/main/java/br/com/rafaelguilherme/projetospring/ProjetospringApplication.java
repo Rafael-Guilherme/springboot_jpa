5593d5fd5d59df13b16f075cfe6ecb314132634e
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-09 09:52:45 -0300

    feat: initial setup

---
+++ src/main/java/br/com/rafaelguilherme/projetospring/ProjetospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelguilherme.projetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringApplication.class, args);
+	}
+
+}
