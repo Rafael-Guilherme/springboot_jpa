e68b2ab40c47edb404fdf6a4ec6f0a916e7b8171
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-09 17:57:50 -0300

    feat: payment one-to-one association

--- src/main/java/br/com/rafaelguilherme/projetospring/config/TestConfig.java
+++ src/main/java/br/com/rafaelguilherme/projetospring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.rafaelguilherme.projetospring.entities.Category;
 import br.com.rafaelguilherme.projetospring.entities.Order;
 import br.com.rafaelguilherme.projetospring.entities.OrderItem;
+import br.com.rafaelguilherme.projetospring.entities.Payment;
 import br.com.rafaelguilherme.projetospring.entities.Product;
 import br.com.rafaelguilherme.projetospring.entities.User;
 import br.com.rafaelguilherme.projetospring.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
